3c0e4fe1d2a028cd6db2ad1d961e81504f5b252b
swag31415 <kidswagatam18@gmail.com>
2020-06-21 18:37:21 -0400

    added node helper methods

--- src/graphs.nim
+++ src/graphs.nim
@@ -8,3 +8,22 @@ type
     score: int
     adj: seq[int] # The id's of nodes they "know"
   Graph* = Table[int, Node] # Just a lookup table for all the nodes
+
+# the node "Fails" the round by setting its score to zero and fail to true
+proc fail*(node: var Node) = 
+  node.score = 0
+  node.fail = true
+
+# the node "Passes" the round by incresing its score and fail to false
+proc pass*(node: var Node) =
+  node.fail = false
+  node.score.inc()
+
+proc get_adj*(node: Node): seq[int] =
+  return node.adj
+
+proc get_score*(node: Node): int =
+  return node.score
+
+proc is_fail*(node: Node): bool =
+  return node.fail
