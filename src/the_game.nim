80f9c51d00874fb13920c534c9229b335a3b2de1
swag31415 <kidswagatam18@gmail.com>
2020-06-21 18:40:00 -0400

    added init and run methods for The_Game

--- src/the_game.nim
+++ src/the_game.nim
@@ -1,2 +1,17 @@
 ## The_Game
 
+import tables
+import sequtils
+import graphs
+
+proc init(graph: var Graph; seed: proc(nodes: openarray[int]): seq[int]) =
+  for id in toSeq(graph.keys).seed():
+    fail(graph[id])
+
+proc run(graph: var Graph; spread: proc(nodes: openarray[int]): seq[int]) =
+  assert(graph.len() > 0) # Assert graph is not empty
+  for node in graph.mvalues:
+    if node.is_fail():
+      for id in node.get_adj().spread():
+        fail(graph[id])
+    pass(node)
